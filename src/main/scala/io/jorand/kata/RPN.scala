93fc8cbb4af9665f82a9d8ba8faf42176e7229cf
Nicolas Jorand <nicolas.jorand@me.com>
2014-07-14 01:05:36 +0200

    Add solution with Multiple errors

--- src/main/scala/io/jorand/kata/RPN.scala
+++ src/main/scala/io/jorand/kata/RPN.scala
@@ -1,24 +1,66 @@
 package io.jorand.kata
 
-import scala.util.Try
+import org.scalactic._
+import Accumulation._
 
 /**
  * @author Nicolas Jorand
  */
 object RPN {
-  
-  implicit class RPNCalculator(s :String) {
 
-    def calc() : Try[BigDecimal] = {
-      Try {
-        s.split(" ").foldLeft(List[BigDecimal]()) {
+  implicit class RPNCalculator(s: String) {
+
+    def parseString(): List[String] Or One[ErrorMessage] = {
+      try {
+        Good(s.split(" ").toList)
+      } catch {
+        case e: Exception => Bad(One(e.getMessage))
+      }
+    }
+
+    def checkHead(stack: List[String]): Validation[ErrorMessage] = {
+      try {
+        BigDecimal(stack(0))
+        BigDecimal(stack(1))
+        Pass
+      } catch {
+        case e: Exception => Fail("The two first items of the queue should contains two numbers")
+      }
+    }
+
+    def checkOperators(stack: List[String]): Validation[ErrorMessage] = {
+      val operators = stack.drop(2).sliding(1, 2).flatten.filter(!"+-*/".contains(_)).toList
+      if (operators.isEmpty) Pass else Fail(s"Invalid character '${operators.foldLeft("")(_ + _)}'")
+    }
+
+    def isNumeric(s: String): Boolean = {
+      try {
+        BigDecimal(s)
+        true
+      } catch {
+        case _ => false
+      }
+    }
+
+    def checkNumbers(stack: List[String]): Validation[ErrorMessage] = {
+      val numbers = stack.drop(3).sliding(1, 2).flatten.filter(!isNumeric(_)).toList
+      if (numbers.isEmpty) Pass else Fail(s"'${numbers.foldLeft("")(_ + _)}' => not number(s)")
+    }
+
+    def calc(): BigDecimal Or Every[ErrorMessage] = {
+      val stack = parseString() when(checkHead, checkOperators, checkNumbers)
+
+      withGood(stack) {
+        _.foldLeft(List[BigDecimal]()) {
           case (x :: y :: xs, "+") => y + x :: xs // Notice the invertion of x and y
           case (x :: y :: xs, "-") => y - x :: xs // Notice the invertion of x and y
           case (x :: y :: xs, "*") => y * x :: xs // Notice the invertion of x and y
           case (x :: y :: xs, "/") => y / x :: xs // Notice the invertion of x and y
-          case(list, num) => BigDecimal(num) :: list
+          case (list, num) => BigDecimal(num) :: list
         }.head
       }
+
     }
   }
+
 }
--- src/test/scala/io/jorand/kata/RPNSpecs.scala
+++ src/test/scala/io/jorand/kata/RPNSpecs.scala
@@ -3,34 +3,40 @@ package io.jorand.kata
 import org.junit.runner.RunWith
 import org.specs2.mutable._
 import org.specs2.runner._
-import scala.util.Success
 import io.jorand.kata.RPN._
+import org.scalactic.{Many, One, Bad, Good}
 
 
 /**
  * Sample specification.
- * 
+ *
  * This specification can be executed with: scala -cp <your classpath=""> io.jorand.kata.SpecsTest
  * Or using maven: mvn test
  *
  * For more information on how to write or run specifications, please visit: 
- *   http://etorreborre.github.com/specs2/guide/org.specs2.guide.Runners.html
+ * http://etorreborre.github.com/specs2/guide/org.specs2.guide.Runners.html
  *
  */
 @RunWith(classOf[JUnitRunner])
 class RPNSpecTest extends Specification {
   "The RPN Calculator" should {
     "perform simple calculation" in {
-      "5 6 +".calc === Success(11)
+      "5 6 +".calc === Good(11)
     }
     "perform simple negative calculation" in {
-      "5 -6 +".calc === Success(-1)
+      "5 -6 +".calc === Good(-1)
     }
     "perform complex calculation calculation" in {
-      "5 6 + 11 / 5 * 2 -".calc === Success(3)
+      "5 6 + 11 / 5 * 2 -".calc === Good(3)
     }
-    "perform simple calculation" in {
-      "5 6 (".calc must not be Success(0)
+    "return error message with a bad operator" in {
+      "5 6 (".calc === Bad(One("Invalid character '('"))
+    }
+    "return error messages with multiple errors" in {
+      "5 6 ( a +".calc === Bad(Many("Invalid character '('","'a' => not number(s)"))
+    }
+    "return an error if the first two items are not a number" in {
+      "a 6 +".calc === Bad(One("The two first items of the queue should contains two numbers"))
     }
 
   }
